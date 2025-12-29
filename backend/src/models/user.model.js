eb05befff089c0e42219e735e1f5408b3cf897dc
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2025-12-29 20:20:19 +0530

    feat: add user model with authentication methods and dependencies

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,80 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    favorites: [
+      {
+        city: { type: String },
+        country: { type: String },
+      },
+    ],
+    recentSearches: [
+      {
+        type: String,
+      },
+    ],
+    preferedUnit: {
+      type: String,
+      enum: ["Celsius", "Fahrenheit"],
+      default: "Celsius",
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = new mongoose.model("User", userSchema);
