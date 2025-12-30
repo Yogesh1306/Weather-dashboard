c8fc692cea0d5d3bfa2b71dc355ce4122515dd42
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2025-12-30 20:36:30 +0530

    feat: add error handling middleware and utility classes for API responses

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import cookieParser from "cookie-parser";
 import express, { json, urlencoded } from "express";
+import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -7,5 +8,6 @@ app.use(urlencoded({ extended: true, limit: "16kb" }));
 app.use(json({ limit: "16kb" }));
 app.use(cookieParser());
 app.use(express.static("public"));
+app.use(errorHandler);
 
 export { app };
